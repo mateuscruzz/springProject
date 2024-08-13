f3fbd7c0cdc39f25871bf0ab6c7a4b367da0954f
Mateus Cruz <mateus.cruz14m@gmail.com>
2024-08-13 13:34:20 -0300

    Order format

---
+++ src/main/java/com/springProject/course/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.springProject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springProject.course.entities.Order;
+import com.springProject.course.entities.User;
+import com.springProject.course.repositories.OrderRepository;
+import com.springProject.course.repositories.UserRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get(); //obj.get porque o get vai retornar o objeto do tipo User que tiver dentro do Optional
+	}
+}
