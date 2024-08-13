5483f87b1a5d68ed737fe36d485a986a49177141
Mateus Cruz <mateus.cruz14m@gmail.com>
2024-08-13 12:20:36 -0300

    Service layer, component registration

---
+++ src/main/java/com/springProject/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.springProject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springProject.course.entities.User;
+import com.springProject.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); //obj.get porque o get vai retornar o objeto do tipo User que tiver dentro do Optional
+	}
+}
