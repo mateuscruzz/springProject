6abafebb7a1a9c41fed6deaba5075707bc14868f
Mateus Cruz <mateus.cruz14m@gmail.com>
2024-08-14 19:12:16 -0300

    payment OneToOne association

--- src/main/java/com/springProject/course/config/TestConfig.java
+++ src/main/java/com/springProject/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.springProject.course.entities.Category;
 import com.springProject.course.entities.Order;
 import com.springProject.course.entities.OrderItem;
+import com.springProject.course.entities.Payment;
 import com.springProject.course.entities.Product;
 import com.springProject.course.entities.User;
 import com.springProject.course.entities.enums.OrderStatus;
@@ -80,5 +81,8 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
