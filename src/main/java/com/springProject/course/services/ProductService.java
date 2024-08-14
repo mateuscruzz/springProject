1e82dd939d272b77ebc60f9d9d052138671be115
Mateus Cruz <mateus.cruz14m@gmail.com>
2024-08-13 21:09:59 -0300

    Products

---
+++ src/main/java/com/springProject/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.springProject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springProject.course.entities.Product;
+import com.springProject.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get(); // obj.get porque o get vai retornar o objeto do tipo Product que tiver dentro do
+							// Optional
+	}
+}
